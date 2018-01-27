37fc44c0c3c27a854b9c837a44e8e5f6146ad57f
zhaozongchang <xingyingsuifeng@gmail.com>
2018-01-27 21:06:51 +0800

    create groups index

---
+++ db/migrate/20180127123441_create_tasks.rb
@@ -0,0 +1,11 @@
+class CreateTasks < ActiveRecord::Migration[5.0]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.text :description
+      t.integer :score
+
+      t.timestamps
+    end
+  end
+end
