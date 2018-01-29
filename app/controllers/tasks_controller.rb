14bfbe679ca0b3c353f43f9ad28b4eb77c3a9cad
zhaozongchang <xingyingsuifeng@gmail.com>
2018-01-29 15:26:28 +0800

    implement task#show

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,4 +7,30 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update
+    redirect_to tasks_path
+  end 
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description, :score)
+  end
+
 end
